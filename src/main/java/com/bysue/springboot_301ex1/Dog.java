8477445b9816c727db9ecb9be55cc5c59a8dc38b
Sue <sueyoung.6311@gmail.com>
2019-03-27 20:34:09 -0400

    Initial commit

---
+++ src/main/java/com/bysue/springboot_301ex1/Dog.java
@@ -0,0 +1,39 @@
+package com.bysue.springboot_301ex1;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private long age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/bysue/springboot_301ex1/Springboot301ex1Application.java
@@ -0,0 +1,13 @@
+package com.bysue.springboot_301ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot301ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot301ex1Application.class, args);
+	}
+
+}
